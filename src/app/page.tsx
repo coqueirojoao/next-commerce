dfd40be2b6cb7ad7f0cd426707f46c306ab03ff4
João Pedro <jaaum.p95@gmail.com>
2024-10-01 16:39:40 -0300

    feat: define tipagem para product

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
-export default function Home() {
+import { ProductType } from "@/types/ProductType";
+
+async function getProducts() {
+  const res = await fetch("https://fakestoreapi.com/products");
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  const data = await res.json();
+
+  return data;
+}
+
+export default async function Home() {
+  const products = await getProducts();
   return (
     <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6"></div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+        {products.map((product: ProductType) => {
+          return (
+            <div key={product.id}>
+              <div>{product.title}</div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
