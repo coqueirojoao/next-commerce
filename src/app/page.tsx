e8346d94a00c6dc71400aeab2f2bc7b55ae4343d
João Pedro <jaaum.p95@gmail.com>
2024-10-01 16:19:36 -0300

    feat: ajusta home para listar produtos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <main>teste</main>;
+  return (
+    <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6"></div>
+    </div>
+  );
 }
